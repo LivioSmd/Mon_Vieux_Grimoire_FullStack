cfb5356cceeefd85027964f767922f1d82a32fb0
Livio Simard <liviosimard@yahoo.fr>
2023-06-26 17:21:50 +0200

    add post book id

--- backend/app.js
+++ backend/app.js
@@ -47,7 +47,13 @@ app.get('/api/books', (req, res, next) => {
     Book.find()
       .then(books => res.status(200).json(books))
       .catch(error => res.status(400).json({ error }));
-  });
+});
+
+app.get('/api/books/:id', (req, res, next) => {
+    Book.findOne({ _id: req.params.id })
+      .then(book => res.status(200).json(book))
+      .catch(error => res.status(404).json({ error }));
+});
 
 app.post('/api/auth/signup', (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
